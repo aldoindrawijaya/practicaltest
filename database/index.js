8810694038b73b7edce2204d1a342dbebe43c670
aldoindrawijaya <aldoindrawijaya11@gmail.com>
2023-06-20 02:07:25 +0700

    deploy vercel

--- database/index.js
+++ database/index.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2")
 const util = require("util")
 
 const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"root",
-    database:"db_practical",
+    host:"bux25htrbuqctwyrtt9q-mysql.services.clever-cloud.com",
+    user:"u2erouxzvl16egut",
+    password:"uxghzQ7L8nfmceIrTTJv",
+    database:"bux25htrbuqctwyrtt9q",
     port : 3306,
 })
 
